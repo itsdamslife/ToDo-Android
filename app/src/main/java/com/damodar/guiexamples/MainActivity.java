c3fcc8fa9af5dfa55f72158b557299814523f36a
Damodar <itsdamslife@gmail.com>
2015-05-28 17:36:31 +0530

    Data persistence across launch

--- app/src/main/java/com/damodar/guiexamples/MainActivity.java
+++ app/src/main/java/com/damodar/guiexamples/MainActivity.java
@@ -1,7 +1,10 @@
 package com.damodar.guiexamples;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,8 +19,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -46,6 +54,8 @@ public class MainActivity extends ActionBarActivity {
 
         favs = new ArrayList<Map>();
 
+        fetchJSONAndFetchData();
+
         favAdapter = new ToDoRowAdapter(this, favs);
 
         ListView theListView = (ListView)findViewById(R.id.theListView);
@@ -60,6 +70,14 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(MainActivity.this, tvShowSelected, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        convertToJSONAndStore();
+
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -110,6 +128,59 @@ public class MainActivity extends ActionBarActivity {
         favs.remove(position);
         favs.add(position, item);
 
+    }
+
+    public void convertToJSONAndStore()
+    {
+        //converting the collection into a JSON
+        JSONArray result= new JSONArray(favs);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("TODOLIST", 0);
+
+        //Storing the string in pref file
+        SharedPreferences.Editor prefEditor = pref.edit();
+        prefEditor.putString("TODOLIST", result.toString());
+        prefEditor.commit();
+    }
+
+    public void fetchJSONAndFetchData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("TODOLIST", 0);
+
+        //Getting the JSON from pref
+        String storedCollection = pref.getString("TODOLIST", null);
+
+        if(storedCollection.length() <= 0) {
+            return;
+        }
+
+        //Parse the string to populate your favs list
+        try {
+
+            JSONArray array = new JSONArray(storedCollection);
+
+            for(int i =0; i<array.length(); i++) {
+
+                JSONObject dict = (JSONObject)array.get(i);
+
+                Map<String, Boolean> aItem = new HashMap<String, Boolean>();
+
+                Iterator<String> keysItr = dict.keys();
+                while(keysItr.hasNext()) {
+
+                    String key = keysItr.next();
+                    Object value = dict.get(key);
+                    Boolean val = Boolean.valueOf(value.toString());
+
+                    aItem.put(key, val);
+                }
+
+                favs.add(aItem);
+            }
+
+
+        } catch (JSONException e) {
+            Log.e("ToDo", "while parsing", e);
+        }
     }
 
 }
