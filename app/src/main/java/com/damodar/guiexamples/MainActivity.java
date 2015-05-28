83aa42c763d8a1d68244dba407caf2aa40101e0b
Damodar <itsdamslife@gmail.com>
2015-05-28 18:12:37 +0530

    Removed Settings option from menu

--- app/src/main/java/com/damodar/guiexamples/MainActivity.java
+++ app/src/main/java/com/damodar/guiexamples/MainActivity.java
@@ -99,12 +99,17 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+//          In menu_main.xml add this
+//        <item android:id="@+id/action_settings" android:title="@string/action_settings"
+//        android:orderInCategory="100" app:showAsAction="never" />
+
 
         return super.onOptionsItemSelected(item);
     }
